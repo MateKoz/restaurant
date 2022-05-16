d89d8deeee165848cb97eae4b55c4407f2a1422a
Mateusz Kozlowsk <climatik@world.pl>
2022-05-16 17:55:31 +0200

    completed check form function

--- js/order.js
+++ js/order.js
@@ -2,10 +2,15 @@ const orderSummary = document.querySelector('.btn__summary-cart');
 const mainFormSummary = document.querySelector('.order');
 const returnBtnSummary = document.querySelector('.return__btn-summary')
 const showPriceInSummary = document.querySelector('.order__p-sum');
-const requiredInput = document.querySelector('#name-order');
 const confirmSummaryCart = document.querySelector('.send-order');
 const inputError = document.querySelector('.order__form-error')
 
+const inputName = document.querySelector('#name-order');
+const inputSurname = document.querySelector('#surname-order');
+const cityInput = document.querySelector('#city-order');
+const addressInput = document.querySelector('#address-order');
+const phoneInput = document.querySelector('#phone-order');
+const payCategory = document.querySelector('#pay-order');
 
 const orderSummaryShowForm = () => {
     mainFormSummary.style.visibility = 'visible';
@@ -13,9 +18,8 @@ const orderSummaryShowForm = () => {
     let sumArrPrice = arrPrice.reduce(function (prev, curr) {
         return prev + curr;
     });
-    showPriceInSummary.textContent = `${sumArrPrice} zł.`
-    shadowCarts.style.display = 'none';
 
+    showPriceInSummary.textContent = `${sumArrPrice} zł.`
     listDishAll.forEach(item => (item.style.display = 'none'));
     iconDish.forEach(item => (item.style.display = 'none'));
     listDish.innerHTML = '';
@@ -24,23 +28,40 @@ const orderSummaryShowForm = () => {
 const returnToMenu = (e) => {
     e.preventDefault();
     mainFormSummary.style.visibility = 'hidden';
+    inputError.style.visibility = 'hidden'
     listDishAll.forEach(item => (item.style.display = 'none'));
     iconDish.forEach(item => (item.style.display = 'flex'));
-    shadowCarts.style.display = 'none';
     listDish.innerHTML = '';
+    clearInputs();
 }
 
 const confirmSummary = (e) => {
     e.preventDefault();
-    console.log(requiredInput.value.length)
-    if(requiredInput.value.length === 0) {
+    if(inputName.value === '' || inputSurname.value === '' || cityInput.value === '' || addressInput.value === '' || phoneInput.value === '' || payCategory.value === 'none') {
         inputError.style.visibility = 'visible'
     } else {
-        console.log('udało sie');
-        inputError.style.visibility = 'hidden'
+        alert('Twoje zamówienie zostało wysłane do realizacji. Dziękujemy');
+        returnToMenu(e);
+        arrPrice = [0];
+        cartShoppingBox.innerHTML = '';
 
+        if (arrPrice.length === 1) {
+            sumPriceCarts.textContent = 'Koszyk jest pusty.';
+            navInfoPrice.textContent = 'Koszyk pusty';
+            tipDelete.textContent = '';
+            btnCompleteOrder.style.display = 'none'
+        }
     }
+}
 
+const clearInputs = () => {
+    inputError.style.visibility = 'hidden'
+    inputName.value = '';
+    inputSurname.value = '';
+    cityInput.value = '';
+    addressInput.value = '';
+    phoneInput.value = '';
+    payCategory.value = 'none';
 }
 
 orderSummary.addEventListener('click', orderSummaryShowForm);
--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const personName = document.querySelector('.header__name-input');
 const errorInputWelcome = document.querySelector('.welcome__form-input-error');
 const cartShop = document.querySelector(".nav__info-right");
 const menuDishes = document.querySelector('.cart__shopping');
-const shadowCarts = document.querySelector(".bgc-shadow");
 const cancelBoxMenu = document.querySelector(".fa-xmark")
 
 // confirm name and set form to display none. localstorage for name in input.
@@ -33,13 +32,11 @@ showName();
 //function opens the list of selected dishes after clicking on the cart in the navigation;
 const showMenu = () => {
     menuDishes.style.display = 'block';
-    shadowCarts.style.display = 'block';
 }
 
 //display none after clicking X btn in menu carts shopping
 const disableCarts = () => {
     menuDishes.style.display = 'none';
-    shadowCarts.style.display = 'none';
 }
 
 //listener
